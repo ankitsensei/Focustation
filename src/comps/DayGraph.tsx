f25d7a178aecc8b39e95195db2bec95baf2d9262
ankitsensei <main.ankitbhagat@gmail.com>
2025-09-03 19:24:57 +0530

    feature added: graph

---
+++ src/comps/DayGraph.tsx
@@ -0,0 +1,52 @@
+import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+
+// Register required components
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const DayGraph = () => {
+    const data = {
+        labels: ["DSA", "Gaming", "Monkeytype", "Video editing", "Web Dev"],
+        datasets: [
+            {
+                label: "Hours",
+                data: [2, 0.5, 0.1, 2, 3],
+                backgroundColor: "#f5865c",
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: { position: "top" },
+            title: { display: true, text: "Time spent this day" },
+        },
+    };
+
+    return (
+        <div>
+            <h1>Graph</h1>
+            <div style={{ width: "600px", margin: "auto" }}>
+                <Bar data={data} options={options} />
+            </div>
+        </div>
+    );
+};
+
+export default DayGraph;
--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,13 +1,16 @@
 import Tasks from "../comps/Tasks"
 import Stats from "../comps/Stats"
 
-import Wall from "../assets/walls/work-lofi-wall.jpg"
+import Wall from "../assets/walls/cat-lofi-wall.jpg"
+import DayGraph from "../comps/DayGraph"
+import WeekGraph from "../comps/WeekGraph"
+import AllTimeGraph from "../comps/AllTimeGraph"
 
 const Hero = () => {
 
   return (
     <div
-      className='flex flex-col pt-20 items-center w-full h-screen bg-cover text-white'
+      className='flex flex-col pt-20 items-center w-full h-screen bg-cover bg-fixed  text-white'
       style={{ backgroundImage: `url(${Wall})` }}
     >
       <h1 className='font-semibold text-6xl mb-8'>Focustation</h1>
@@ -17,6 +20,19 @@ const Hero = () => {
       </div>
       <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
         <Stats />
+      </div>
+      <div>
+        <div className="flex gap-2">
+          <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
+            <DayGraph />
+          </div>
+          <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
+            <WeekGraph />
+          </div>
+        </div>
+        <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
+          <AllTimeGraph />
+        </div>
       </div>
     </div>
   )

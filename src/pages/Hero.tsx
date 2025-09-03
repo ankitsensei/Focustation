f85b9f27808d9d1791dfa4c4f5b6ade9098b6739
ankitsensei <main.ankitbhagat@gmail.com>
2025-09-03 22:52:43 +0530

    fixed: grid layout... removed: stats file

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,5 +1,4 @@
 import Tasks from "../comps/Tasks"
-import Stats from "../comps/Stats"
 
 import Wall from "../assets/walls/cat-lofi-wall.jpg"
 import DayGraph from "../comps/DayGraph"
@@ -10,29 +9,29 @@ const Hero = () => {
 
   return (
     <div
-      className='flex flex-col pt-20 items-center w-full h-full bg-cover bg-fixed  text-white hide-scrollbar'
+      className='flex flex-col pt-20 items-center w-full min-h-screen bg-cover bg-fixed  text-white hide-scrollbar'
       style={{ backgroundImage: `url(${Wall})` }}
     >
       <h1 className='font-semibold text-6xl mb-8'>Focustation</h1>
 
-      <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
-        <Tasks />
-      </div>
-      <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
-        <Stats />
-      </div>
       <div>
-        <div className="flex gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 items-center">
           <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
             <DayGraph />
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
+              <Tasks />
+            </div>
           </div>
           <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
             <WeekGraph />
           </div>
+          <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
+            <AllTimeGraph />
+          </div>
         </div>
-        <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl p-6 shadow-lg">
-          <AllTimeGraph />
-        </div>
+
       </div>
     </div>
   )
